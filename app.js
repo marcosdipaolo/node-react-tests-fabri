9c1175578b802ccb63686129fb94531f8c2ed719
fabri2605 <fabridp77@gmail.com>
2022-06-28 12:21:49 -0300

    dinamic_also_like

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ app.use(express.static('public'));
 const routes = require('./src/routes/homeRoutes');
 app.use(routes);
 app.use((req, res) => {
-    res.status(404).render('./errors/index', { title: 'Error' });
+    res.status(404).render('./errors/index', { title: 'Error', desc: 'Page not found!' });
 });
 
 //listen
 app.listen(3000, () => {
     console.log('app running at port 3000');
 });
+
+/* <% if ( events.length > 0 ){ events.forEach( e =>{ %>
+    <div>
+        <h1><%= e.title %></h1>
+    </div>
+    <% })}else { %>
+    <h1 class="d-flex justify-content-center p-3">No events to display</h1> */
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,26 +1,47 @@
+const fetch = require('node-fetch');
 const controller = {};
 
+const url = 'https://react-http-467cc-default-rtdb.firebaseio.com/events.json';
 controller.index = (req, res) => {
     res.render('./home/index', { root: __dirname, title: 'Home' });
 };
-controller.about = (req, res) => {
-    res.render('./about/index', { root: __dirname, title: 'About'  });
+controller.about = async (req, res) => {
+    try {
+        const response = await fetch(url);
+        await response.json()
+            .then((imp)=>{
+                let evs = [];
+                for(const key in imp){
+                    evs.push(imp[key])
+                }
+                res.render('./about/index', {
+                    root: __dirname,
+                    title: 'About',
+                    events: evs,
+                })}
+            );
+    } catch (e) {
+        res.status(404).render('./errors/index', {
+            title: 'Error',
+            desc: e.message + '!',
+        });
+    }
 };
 controller.rent_venue = (req, res) => {
-    res.render('./rent_venues/index', { root: __dirname, title: 'Rent Venue'  });
+    res.render('./rent_venues/index', { root: __dirname, title: 'Rent Venue' });
 };
 //events
 controller.show_events = (req, res) => {
-    res.render('./show_events/index', { root: __dirname, title: 'Events'  });
+    res.render('./show_events/index', { root: __dirname, title: 'Events' });
 };
 controller.event_details = (req, res) => {
-    res.render('./event_details/index', { root: __dirname, title: 'Details'  });
+    res.render('./event_details/index', { root: __dirname, title: 'Details' });
 };
 //tickets
 controller.tickets = (req, res) => {
-    res.render('./tickets/index', { root: __dirname, title: 'Tickets'  });
+    res.render('./tickets/index', { root: __dirname, title: 'Tickets' });
 };
 controller.ticket_details = (req, res) => {
-    res.render('./tickets/details', { root: __dirname, title: 'Details'  });
+    res.render('./tickets/details', { root: __dirname, title: 'Details' });
 };
 module.exports = controller;
